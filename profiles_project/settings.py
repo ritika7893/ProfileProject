c8a9a28586789be4d7a025ee6617f3415e267f55
ritika7893 <ritikakumari87344@gmail.com>
2025-04-17 12:19:35 +0530

    User Profile Model is Added  commit

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xo#(%0hyh88l^y%!5sic-oc-azgn3^u3e1mc#8w0o72vp=3!+4"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# In settings.py
+AUTH_USER_MODEL = "profiles_api.UserProfile"
